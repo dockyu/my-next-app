54c91e02c2e08fcd6cbd782ce12754238df4fe5e
dockyu <dockyu.chen@gmail.com>
2023-07-29 07:41:16 -0700

    use src and Absolute Imports

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
     // ...
     eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
     },
     typescript: {
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      ignoreBuildErrors: true,
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
     },
-  };
+};
 
 module.exports = nextConfig;
